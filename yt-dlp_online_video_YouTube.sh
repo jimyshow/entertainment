e8ef35417e21937a341f99282007e6097522ab7f
jimyshow <jimyshow@gmail.com>
2025-05-27 16:07:10 +0800

    Update yt-dlp_online_video_YouTube.sh

--- yt-dlp_online_video_YouTube.sh
+++ yt-dlp_online_video_YouTube.sh
@@ -1,11 +1,10 @@
+
+./yt-dlp -f140 --playlist-start 134 --playlist-end 135 --download-archive GuoLawMay20.txt https://www.youtube.com/playlist?list=PLL_n-Bh7cVgVfDPPjBwDtTqelcIzrEZkU
+
+.\yt-dlp.exe -f 233+602 https://www.youtube.com/watch?v=Ka7aJb4muEY --verbose
+
 ./yt-dlp -f140 --download-archive GuoLawMay20.txt https://www.youtube.com/playlist?list=PLL_n-Bh7cVgVfDPPjBwDtTqelcIzrEZkU
 
-234 mp4   audio only        │                  m3u8  │ audio only          unknown             Default
-140 m4a   audio only      2 │  170.54MiB  129k https │ audio only          mp4a.40.2  129k 44k medium, m4a_dash
-251 webm  audio only      2 │  142.58MiB  108k https │ audio only          opus       108k 48k medium, webm_dash
-269 mp4   144x256     30    │ ~251.55MiB  191k m3u8  │ avc1.4D400C    191k video only
-160 mp4   144x256     30    │   87.74MiB   67k https │ avc1.4d400c     67k video only          144p, mp4_dash
-230 mp4   360x640     30    │ ~880.71MiB  669k m3u8
 
 .\yt-dlp.exe   -f140                              https://www.youtube.com/watch?v=5Xm7R-U5WGw
 .\yt-dlp.exe   -f 'ba' -x --audio-format mp3      https://www.youtube.com/watch?v=eGFi2Dh1XLc -o '%(id)s.mp3'
